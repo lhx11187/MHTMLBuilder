e74778b30dff4dbf33dbd67e740bab5e6a59bee8
Christian Junk <christian.junk@geomatics.io>
2019-10-10 23:18:47 +0200

    Initial Commit.

---
+++ src/MHTMLBuilder.Tests/CoreUnitTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace MHTMLBuilder.Tests
+{
+    public class CoreUnitTests
+    {
+        private Builder MHTMLBuilder = new Builder();
+        private String URL = "https://www.google.com";
+        private String fileLocation = "/tmp/test.mht";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SavePageArchive()
+        {
+            String path = String.Empty;
+
+            try
+            {
+                path = MHTMLBuilder.SavePageArchive(fileLocation, Builder.FileStorage.DiskPermanent, URL);
+                Assert.IsTrue(File.Exists(path));
+
+            }
+            catch (CustomException.BuilderInvalidFileNameException e)
+            {
+                Console.WriteLine("BuilderInvalidFileNameException is " + e.CustomMessage);
+            }
+            catch (CustomException.BuilderDownLoadHTMLException e)
+            {
+                Console.WriteLine("BuilderDownLoadHTMLException " + e.CustomMessage);
+            }
+
+            Assert.Pass();
+        }
+    }
+}
